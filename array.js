06d60a9bd1cedc43b5cad5b41acde69b40c8ea10
남세환 <andrewnam@namsehwan-ui-Macmini.local>
2022-03-17 21:19:27 +0900

    init

---
+++ array.js
@@ -0,0 +1,14 @@
+console.log('hello hello');
+
+const hey = (a, b) => {
+    let c = a+b;
+    return c
+}
+
+console.log(`test : ${hey(3,5)}`);
+
+const fruits = [4, 10, 30, 50, 60];
+
+for (const fruit of fruits){
+    console.log(`과일 : ${fruit}`);
+}
